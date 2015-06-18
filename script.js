20f567f5a39b94d34398f6715aece98c6a48226b
Kyösti Komulainen <kyosti.komulainen@arcusys.fi>
2015-06-18 17:51:12 +0300

    Add script

---
+++ script.js
@@ -0,0 +1,11 @@
+var n = $('ul[role="menubar"]').children().length;
+if (n % 2 === 0) {
+    for (i = 0; i < n / 2 - 1; i++) {
+        console.log("hyy");
+    }
+ else {
+    
+ }
+}
+
+console.log(n);
