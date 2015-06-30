4dcf8e6bbc1ef2ec996676eb79908f7129e0bacb
Kyösti Komulainen <kkomulainen@gmail.com>
2015-06-30 23:06:17 +0300

    Alternate solution for submenu

---
+++ scrap.js
@@ -0,0 +1,52 @@
+// submenu-center
+// submenu-onleft
+// submenu-leftest
+// submenu-onright
+// submenu-rightest
+
+var n = $('ul[role="menubar"]').children().length;
+console.log(n);
+
+// Mark one or two menu items as center
+if (n % 2 !== 0) {
+    var c = Math.round(n/2);
+    $('ul[role="menubar"] > li:nth-child(' + c + ')').addClass("submenu-center");
+}
+else {
+    var c1 = n/2;
+    var c2 = c1+1;
+    $('ul[role="menubar"] > li:nth-child(' + c1 + ')').addClass("submenu-center");
+    $('ul[role="menubar"] > li:nth-child(' + c2 + ')').addClass("submenu-center");
+}
+
+$('ul[role="menubar"] > li:nth-child(1)').addClass("submenu-leftest");
+$('ul[role="menubar"] > li:nth-child(' + n + ')').addClass("submenu-rightest");
+
+if (n % 2 === 0) {
+    for (i = 0; i < n / 2 - 1; i++) {
+        console.log("hyy");
+    }
+}
+
+
+var w = $('.has-children:nth-child(1) > ul').css( "width" );
+$('.has-children:nth-child(1) > ul').addClass("assdalkjasdjllkas");
+console.log(w);
+
+var w = $('.has-children:nth-child(2) > ul').width();
+console.log(w);
+
+var w = $('.has-children:nth-child(3) > ul').width();
+console.log(w);
+
+var w = $('.has-children:nth-child(4) > ul').width();
+console.log(w);
+
+var w = $('.has-children:nth-child(5) > ul').width();
+console.log(w);
+
+var w = $('.has-children:nth-child(6) > ul').width();
+console.log(w);
+
+var w = $('.has-children:nth-child(1) > ul').children().length;
+console.log(w);
