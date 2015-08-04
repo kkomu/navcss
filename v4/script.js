75d254fc79910c91bbd7f1fad774b9480e58f63b
Kyösti Komulainen <kkomulainen@gmail.com>
2015-08-04 21:45:14 +0300

    Position submenu in all scenarios

--- v4/script.js
+++ v4/script.js
@@ -1,45 +1,42 @@
-$(document).ready(function() {
-    var mainMenuWidth = $('nav#navigation').width();
-    var subMenuWidth = parseInt($('ul.child-menu').css("min-width"));
-    var mainMenuHeight = $('nav#navigation').height();
-    $('ul.child-menu').css("top", mainMenuHeight);
-    
+(function() {
+    'use strict';
+
     $('ul[role="menubar"] > li.has-children').hover(function() {
-        var menuItemWidth = $(this).position().left;
-        var subMenuOffset = (mainMenuWidth/2)-(subMenuWidth/2)-menuItemWidth;
-        $(this).children('ul.child-menu').css("left", subMenuOffset);
-        $(this).children('ul.child-menu').css("display", "table");
+        setSubmenuPosition(this);
+        $(this).children('ul.child-menu').css("display", "block");
     }, function() {
         $(this).children('ul.child-menu').css("display", "none");
     });
 
+    $('ul[role="menubar"] > li.has-children').click(function() {
+        if(typeof(Storage) !== "undefined") {
+            var pageName = $(this).children('a').text();
+            localStorage.setItem("clickedPage", pageName);
+        } 
+    });
+    
+    $(window).load(function () {
+        if(typeof(Storage) !== "undefined") {
+            var pageName = localStorage.getItem("clickedPage");
+            if(pageName !== null) {
+                localStorage.removeItem("clickedPage");
+                console.log(pageName);
+                var menuElement = $('li:contains('+ pageName +')');
+                //setSubmenuPosition(menuElement);
+                //$(menuElement).children('ul.child-menu').css("display", "block");
+            }
+        }
+    });
+    
+    function setSubmenuPosition(that) {
+        var mainMenuHeight = $('nav#navigation').height();
+        $(that).children('ul.child-menu').css("top", mainMenuHeight);
 
-});
+        var mainMenuWidth = $('nav#navigation').width();
+        var subMenuWidth = parseInt($('ul.child-menu').css("width")); 
+        var menuItemWidth = $(that).position().left;
+        var subMenuOffset = (mainMenuWidth/2)-(subMenuWidth/2)-menuItemWidth;
+        $(that).children('ul.child-menu').css("left", subMenuOffset);
+    }
 
-//$( window ).load(setSubmenuOffset);
-//$( window ).load(setSubmenuVerticalPosition);
-//$( window ).resize(setSubmenuOffset);
-
-/**
- * setSubMenuOffset
- * Horizontally center submenu when screen size changes
- */
-function setSubmenuOffset() {
-    var mainMenuWidth = $('nav#navigation').width();
-    //console.log("mainMenuWidth: " + mainMenuWidth);
-    var subMenuWidth = parseInt($('ul.child-menu').css("min-width"));
-    //console.log("mainMenuWidth: " + mainMenuWidth);
-    var subMenuOffset = (mainMenuWidth/2)-(subMenuWidth/2);
-    //console.log("subMenuOffset: " + subMenuOffset);
-    $('ul.child-menu').css("left", subMenuOffset);
-}
-
-/**
- * setSubmenuVerticalPosition
- * Vertically position submenu
- */
-function setSubmenuVerticalPosition() {
-    var mainMenuHeight = $('nav#navigation').height();
-    //console.log("mainMenuHeight: " + mainMenuHeight);
-    $('ul.child-menu').css("top", mainMenuHeight);
-}
+})();
