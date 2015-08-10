651f0b13e09a9cf531eb140852a523a110876307
Kyösti Komulainen <kyosti.komulainen@arcusys.fi>
2015-08-10 08:53:57 +0300

    Different alignment to child-menu

--- v5/script.js
+++ v5/script.js
@@ -1,29 +1,37 @@
-(function() {
+$( document ).ready(function() {
     'use strict';
-    
+
+    $(window).resize(setMainMenuWidth);
+    $(window).resize(showClickedMenu);
+    $(window).load(setMainMenuWidth);
+    $(window).load(showClickedMenu);
+
     // Liferay uses class 'selected' on menu item that has been selected
-    // However the class is always added to first page
-    // We don't want to show the submenu when we first come to the page
-    
+    // We'll show the child-menu whenever main menu item has class '.selected'
+    // However the the class 'selected' is added to main page by default
+    // We don't want to show the child-menu when we visit the page first time
+    // Local storage is used to store value when main page has actually been clicked
+
     // Handle hovering main menu items
-    $('ul[role="menubar"] > li.has-children').hover(function() {
-        // Hide submenu of selected menu item
+    $('nav#navigation > ul[role="menubar"] > li.has-children').hover(function() {
+        // Hide submenu of '.selected menu' item
         var menuElement = $('li.has-children.selected');
         $(menuElement).children('ul.child-menu').css("display", "none");
-        $(menuElement).children('.navi-arrow').css("display", "none");   
+        $(menuElement).children('.navi-arrow').css("display", "none");
         // Show submenu of hovered menu item
         setNaviArrowPosition(this);
         $(this).children('.navi-arrow').css("display", "inline");
-        $(this).children('ul.child-menu').css("display", "table");
+        $(this).children('ul.child-menu').css("display", "block");
     }, function() {
         // Hide submenu of hovered menu item
         $(this).children('.navi-arrow').css("display", "none");
         $(this).children('ul.child-menu').css("display", "none");
-        // Show submenu of selected menu item
-        var menuElement = $('ul[role="menubar"] > li.selected.has-children');
+        // Show submenu of '.selected' menu item
+        var menuElement = $('nav#navigation > ul[role="menubar"] > li.selected.has-children');
+        // If it's not the main page then just show '.selected' page
         if( menuElement.is(':not(:first-child)')) {
             $(menuElement).children('.navi-arrow').css("display", "inline");
-            $(menuElement).children('ul.child-menu').css("display", "table");
+            $(menuElement).children('ul.child-menu').css("display", "block");
         }
         // Check if first page is really selected
         else {
@@ -32,72 +40,64 @@
                 if(pageName !== null) {
                     setNaviArrowPosition(menuElement);
                     $(menuElement).children('.navi-arrow').css("display", "inline");
-                    $(menuElement).children('ul.child-menu').css("display", "table");
+                    $(menuElement).children('ul.child-menu').css("display", "block");
                 }
             }
-        } 
+        }
     });
-    
+
     // Save variable to local storage if first menu item is clicked
-    $('ul[role="menubar"] > li.has-children').click(function() {
+    $('nav#navigation > ul[role="menubar"] > li.has-children').click(function() {
         if(typeof(Storage) !== "undefined") {
             localStorage.removeItem("mainPageClicked");
             if ($(this).is(':first-child')) {
                 if(typeof(Storage) !== "undefined") {
                     localStorage.setItem("mainPageClicked", "1");
-                } 
+                }
             }
         }
     });
-    
-    $(window).resize(setMainMenuWidth);
-    $(window).load(setMainMenuWidth);
-    
+
     // Wrap main menu item's text when screen get smaller
     function setMainMenuWidth() {
-        var width = 0;
-        var count = $('ul[role="menubar"] > li').size();
-        var masa = $('ul[role="menubar"]').outerWidth();
-        $('ul[role="menubar"] > li').each(function() {
-            width += $(this).width();
+        var menuItemsWidth = 0;
+        var menuItemCount = $('nav#navigation > ul[role="menubar"] > li').size();
+        var naviWidth = $('nav#navigation > ul[role="menubar"]').outerWidth();
+        $('nav#navigation > ul[role="menubar"] > li').each(function() {
+            menuItemsWidth += $(this).width();
         });
-        
-        console.log(width);
-        console.log(masa);
-        
-        if(width > masa) {
-            $('ul[role="menubar"] > li').each(function() {
-                var widd = Math.round(95 / count);
-                console.log(widd);
-                $(this).css("width",widd+"%");
+        if(menuItemsWidth > naviWidth) {
+            $('nav#navigation > ul[role="menubar"] > li').each(function() {
+                var itemWidth = Math.round(95 / menuItemCount);
+                console.log(itemWidth);
+                $(this).css("width",itemWidth+"%");
             });
         }
         else {
-             $('ul[role="menubar"] > li').each(function() {
+            $('nav#navigation > ul[role="menubar"] > li').each(function() {
                 $(this).css("width","initial");
-             });
+            });
         }
     }
-    $(window).resize(showClickedMenu);
-    $(window).load(showClickedMenu);
-    
-    // Keep submenu 
+
+
+    // Show child-menu under '.selected' main menu item
     function showClickedMenu() {
         // Show child-menu when it's parent has selected-class UNLESS it's the main page
-        var selectedElement = $('ul[role="menubar"] > li.selected.has-children');
+        var selectedElement = $('nav#navigation > ul[role="menubar"] > li.selected.has-children');
         if( selectedElement.is(':not(:first-child)')) {
             setNaviArrowPosition(selectedElement);
-            $(selectedElement).children('ul.child-menu').css("display", "table");
+            $(selectedElement).children('ul.child-menu').css("display", "block");
             $(selectedElement).children('.navi-arrow').css("display", "inline");
         }
-        // Show main page child-menu ONLY when it's selected from navigation 
+        // Show main page child-menu ONLY when it's clicked from navigation
         else {
             if(typeof(Storage) !== "undefined") {
                 var pageName = localStorage.getItem("mainPageClicked");
                 if(pageName !== null) {
-                    var menuElement = $('ul[role="menubar"] > li.has-children.selected');
+                    var menuElement = $('nav#navigation > ul[role="menubar"] > li.has-children.selected');
                     setNaviArrowPosition(menuElement);
-                    $(menuElement).children('ul.child-menu').css("display", "table");
+                    $(menuElement).children('ul.child-menu').css("display", "block");
                     $(menuElement).children('.navi-arrow').css("display", "inline");
                 }
                 else {
@@ -105,10 +105,10 @@
                     $('.navi-arrow').css("display", "none");
                 }
             }
-        }        
+        }
     };
-    
-    // Position submenu and navi-arrow correctly
+
+    // Position  navi-arrow correctly
     function setNaviArrowPosition(that) {
         // Center navi-arrow horizontally
         var menuItemPosition = $(that).position().left;
@@ -116,5 +116,4 @@
         var naviArrowOffset = Math.round(menuItemPosition + menuItemWidth/2);
         $(that).children('.navi-arrow').css("left",naviArrowOffset);
     }
-
-})();
+});
